f1fe62b3073b0f3061bebb912e56f3010c97213b
teloe <tomeloe@gmail.com>
2019-07-05 08:49:04 -0700

    add liebertz

---
+++ liebertz/js/custom.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+
+	$('.stellarnav').stellarNav({
+		showArrows:false,
+		breakpoint:960,
+		menuLabel:'Menu',
+		phoneBtn:'',
+		locationBtn:''
+	});
+
+	$('#slider').flexslider({
+	    animation: "fade",
+	    directionNav: false,
+	    controlNav: false
+	});
+});
