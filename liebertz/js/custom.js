3445a858355186b8a2eb73a5f83295afd4e7e890
teloe <tomeloe@gmail.com>
2019-07-09 14:20:47 -0700

    more styles/ complete nav/ begin media queries

--- liebertz/js/custom.js
+++ liebertz/js/custom.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 
 	$('.stellarnav').stellarNav({
 		showArrows:false,
-		breakpoint:960,
+		// breakpoint:960,
 		menuLabel:'Menu',
 		phoneBtn:'',
 		locationBtn:''
@@ -12,5 +12,25 @@ jQuery(document).ready(function($) {
 	    animation: "fade",
 	    directionNav: false,
 	    controlNav: false
-	});
+    });
+
+    if ($(window).width() < 768) {
+        $('.stellarnav').removeClass('mobile').addClass('desktop');
+     }
+
+    // remove mobile stellarnav on window resize
+    $(window).resize(function() {
+        if ($(window).width() < 768) {
+            $('.stellarnav').removeClass('mobile').addClass('desktop');
+         }
+    });
+
+    // hamburger menu open/ close
+    $('.hamburger').on('click', function(){
+        $('nav').css('transform', 'translateX(0)');
+    });
+    $('.close-btn').on('click', function() {
+        $('nav').css('transform', 'translateX(500px)');
+    });
+
 });
