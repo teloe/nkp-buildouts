30706c61162355a50ce2a43e273cb16a75c49754
teloe <tomeloe@gmail.com>
2019-07-29 14:54:27 -0700

    style updates/ add images/ begin responsive styles

--- lahiri/js/custom.js
+++ lahiri/js/custom.js
@@ -1,17 +1,40 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-	$('.stellarnav').stellarNav({
-		showArrows:false,
-		breakpoint:960,
-		menuLabel:'Menu',
-		phoneBtn:'',
-		locationBtn:''
-	});
+    $('.stellarnav').stellarNav({
+        showArrows: false,
+        breakpoint: 960,
+        menuLabel: 'Menu',
+        phoneBtn: '',
+        locationBtn: ''
+    });
 
-	// $('#slider').flexslider({
-	//     animation: "fade",
-	//     directionNav: false,
-	//     controlNav: false
+    // Sticky nav
+    window.onscroll = function() {
+        navScroll();
+    };
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+
+    function navScroll() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+
+    var loc = window.location.href;
+
+    $('.stellarnav').find('a').each(function () {
+        $(this).toggleClass('active', $(this).attr('href') == loc);
+    });
+
+    // $('#slider').flexslider({
+    //     animation: "fade",
+    //     directionNav: false,
+    //     controlNav: false
     // });
-    
+
 });
