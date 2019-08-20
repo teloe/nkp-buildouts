ec65551cb821f12c6b1b1f8781765884c669e031
teloe <tomeloe@gmail.com>
2019-08-20 15:05:33 -0700

    style/ content updates

--- nashielli/js/custom.js
+++ nashielli/js/custom.js
@@ -2,15 +2,21 @@ jQuery(document).ready(function($) {
     $('.stellarnav').stellarNav({
         showArrows: false,
         breakpoint: 999999,
-        menuLabel: 'Menu',
         phoneBtn: '',
         locationBtn: '',
         mobileMode: true
     });
 
-    $('#slider').flexslider({
-        animation: 'fade',
-        directionNav: false,
-        controlNav: false
-    });
+    // function openNav() {
+    //     $("").css('transform', 'translateX(0px)') ;
+    // }
+    // function closeNav() {
+    //     $("").style.transform = "translateX(100%)";
+    // }
+
+    // $('#slider').flexslider({
+    //     animation: 'fade',
+    //     directionNav: false,
+    //     controlNav: false
+    // });
 });
