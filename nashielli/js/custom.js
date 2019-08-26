31ec28e2dcc9ee92d07d02bec4670fa320e6291d
teloe <tomeloe@gmail.com>
2019-08-26 14:54:01 -0700

    style updates

--- nashielli/js/custom.js
+++ nashielli/js/custom.js
@@ -1,32 +1,38 @@
-jQuery(document).ready(function($) {
-    $('.stellarnav').stellarNav({
+jQuery(document).ready(function ($) {
+    $(".stellarnav").stellarNav({
         showArrows: false,
         breakpoint: 999999,
-        phoneBtn: '',
-        locationBtn: '',
+        phoneBtn: "",
+        locationBtn: "",
         mobileMode: true
     });
 
-    $('.testimonials-slider').flexslider({
-        animation: 'fade',
+    // hamburger menu open/ close
+    $('.open-nav').on('click', function(){
+        $('nav').css('transform', 'translateX(0)');
+    });
+    $('.closebtn').on('click', function() {
+        $('nav').css('transform', 'translateX(300px)');
+    });
+
+    $(".testimonials-slider").flexslider({
+        animation: "fade",
         directionNav: true,
         controlNav: false,
         customDirectionNav: "#testimonials-direction-nav li a"
     });
 
-    $('.office-slider').flexslider({
-        animation: 'fade',
+    $(".office-slider").flexslider({
+        animation: "fade",
         directionNav: true,
         controlNav: false,
         customDirectionNav: "#office-direction-nav li a"
     });
 
-    // function openNav() {
-    //     $("").css('transform', 'translateX(0px)') ;
-    // }
-    // function closeNav() {
-    //     $("").style.transform = "translateX(100%)";
-    // }
+    $(".back-to-top").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+        return false;
+      });
 
     // $('#slider').flexslider({
     //     animation: 'fade',
