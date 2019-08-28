096af0eb6f6b496236ac4c09e094c4519a34b43c
teloe <tomeloe@gmail.com>
2019-08-28 16:27:44 -0700

    add images/ html markup

--- comstock/js/custom.js
+++ comstock/js/custom.js
@@ -1,16 +1,17 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-	$('.stellarnav').stellarNav({
-		showArrows:false,
-		breakpoint:960,
-		menuLabel:'Menu',
-		phoneBtn:'',
-		locationBtn:''
-	});
+    $('.stellarnav').stellarNav({
+        showArrows: false,
+        breakpoint: 960,
+        menuLabel: 'Menu',
+        phoneBtn: '520-797-8885',
+        locationBtn: 'https://goo.gl/maps/jRcWVdX9RbrBJp5x6'
+    });
 
-	$('#slider').flexslider({
-	    animation: "fade",
-	    directionNav: false,
-	    controlNav: false
-	});
+    /* $('#slider').flexslider({
+        animation: 'fade',
+        directionNav: false,
+        controlNav: false
+    }); */
+
 });
