1690c32749bd074f37b0697f55738df9bfbf8805
teloe <tomeloe@gmail.com>
2019-09-05 13:52:51 -0700

    final homepage style changes/ content/ etc.

--- pinzone/js/custom.js
+++ pinzone/js/custom.js
@@ -8,6 +8,21 @@ jQuery(document).ready(function ($) {
         locationBtn: ''
     });
 
+    var navTop = $('nav').offset().top;
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > navTop ) {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    });
+
+    $('.testimonials-slider').flexslider({
+        animation: 'fade',
+        directionNav: false,
+        controlNav: false
+    });
+
     /* $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
