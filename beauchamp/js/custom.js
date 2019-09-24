dc1540509e313105536212495b1af63d5f607871
teloe <tomeloe@gmail.com>
2019-09-24 10:52:31 -0700

    final style changes to homepage

--- beauchamp/js/custom.js
+++ beauchamp/js/custom.js
@@ -1,19 +1,26 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'334-558-0262',
-        locationBtn:'https://goo.gl/maps/HaRorjqrWnVdguUE7'
+        showArrows: false,
+        breakpoint: 960,
+        menuLabel: 'Menu',
+        phoneBtn: '334-558-0262',
+        locationBtn: 'https://goo.gl/maps/HaRorjqrWnVdguUE7'
     });
 
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         if ($(this).scrollTop() > 30) {
             $('header').addClass('sticky');
         } else {
             $('header').removeClass('sticky');
         }
+    });
+
+    $('.bxa-slider').flexslider({
+        animation: 'fade',
+        directionNav: false,
+        controlNav: true,
+        manualControls: ".custom-control--nav li"
     });
 
     /* $('#slider').flexslider({
