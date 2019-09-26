7ee7ce9865ab5368a2d8f9258c6abdefb74a1a31
teloe <tomeloe@gmail.com>
2019-09-26 11:50:44 -0700

    final homepage style edits

--- cohen-barry/js/custom.js
+++ cohen-barry/js/custom.js
@@ -2,7 +2,8 @@ jQuery(document).ready(function ($) {
 
     $('.stellarnav').stellarNav({
         showArrows: false,
-        breakpoint: 960,
+        mobileMenu: true,
+        breakpoint: 9999,
         menuLabel: 'Menu',
         phoneBtn: '',
         locationBtn: ''
@@ -26,10 +27,17 @@ jQuery(document).ready(function ($) {
 
     $('.testimonials-slider').flexslider({
         animation: 'fade',
-        directionNav: false,
-        controlNav: false,
-        // manualControls
+        directionNav: true,
+        controlNav: true,
+        prevText: '',
+        nextText: ''
+        // manualControls: '.custom-nav--controls li a' 
     });
+
+    $('.testimonials-slider .flex-prev').append('<i class="fas fa-chevron-left"></i>');
+    $('.testimonials-slider .flex-next').append('<i class="fas fa-chevron-right"></i>');
+
+    $('.flex-control-nav a').empty();
     
     /* $('#slider').flexslider({
         animation: 'fade',
