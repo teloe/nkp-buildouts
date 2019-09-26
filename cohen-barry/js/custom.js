519f5a372db6581cafbb6744dc05da478a6aca65
teloe <tomeloe@gmail.com>
2019-09-25 21:23:49 -0700

    styles up to .services section

--- cohen-barry/js/custom.js
+++ cohen-barry/js/custom.js
@@ -8,13 +8,29 @@ jQuery(document).ready(function ($) {
         locationBtn: ''
     });
 
+    $('.open-nav').on('click', function() {
+        $('#main-nav').css('transform', 'translateX(0)');
+        $('#nav-overlay-close').css({
+            'opacity': 1,
+            'visibility': 'visible'
+        });
+    });
+
+    $('.close-btn, #nav-overlay-close').on('click', function () {
+        $('#main-nav').css('transform', 'translateX(450px)');
+        $('#nav-overlay-close').css({
+            'opacity': 0,
+            'visibility': 'hidden'
+        });
+    });
+
     $('.testimonials-slider').flexslider({
         animation: 'fade',
         directionNav: false,
         controlNav: false,
         // manualControls
     });
-
+    
     /* $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
