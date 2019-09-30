6b21b0f7516036212d6156fa04c069d4f706a403
teloe <tomeloe@gmail.com>
2019-09-30 14:51:46 -0700

    style up to .testimonials section

--- carman-timothy/js/custom.js
+++ carman-timothy/js/custom.js
@@ -2,11 +2,43 @@ jQuery(document).ready(function($) {
 
     $('.stellarnav').stellarNav({
         showArrows:false,
-        breakpoint:960,
+        breakpoint:767,
         menuLabel:'Menu',
         phoneBtn:'',
         locationBtn:''
     });
+
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 0 ) {
+            $('header').addClass('sticky');
+        } else {
+            $('header').removeClass('sticky');
+        }
+    });
+
+    $('.banner-slider').flexslider({
+        animation: 'fade',
+        directionNav: true,
+        controlNav: false,
+        prevText: '',
+        nextText: '',
+        slideshowSpeed: 5000,
+    });
+
+    $('.testimonials-slider').flexslider({
+        animation: 'fade',
+        directionNav: true,
+        controlNav: false,
+        prevText: '',
+        nextText: ''
+    });
+
+    $('#slider .flex-prev').html('<img class="img-resp" src="../images/prev.png">');
+    $('#slider .flex-next').html('<img class="img-resp" src="../images/next.png">');
+
+    $('.testimonials .flex-prev').html('<i class="fas fa-chevron-left"></i>');
+    $('.testimonials .flex-next').html('<i class="fas fa-chevron-right"></i>');
+
 
     /* $('#slider').flexslider({
         animation: 'fade',
