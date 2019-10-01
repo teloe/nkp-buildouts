efd76050efabc20d9795743ab7fd101363a57ee5
teloe <tomeloe@gmail.com>
2019-10-01 11:27:50 -0700

    add back-to-top button

--- carman-timothy/js/custom.js
+++ carman-timothy/js/custom.js
@@ -36,6 +36,19 @@ jQuery(document).ready(function($) {
     $('#slider .flex-prev').html('<img class="img-resp" src="../images/prev.png">');
     $('#slider .flex-next').html('<img class="img-resp" src="../images/next.png">');
 
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 400) {
+            $('.to-top').fadeIn(200);
+        } else {
+            $('.to-top').fadeOut(200);
+        }
+    });
+
+    $('.to-top').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 'ease');
+    });
+
     /* $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
