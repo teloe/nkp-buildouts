2e98dd54b77fb3b6235f5c34e81785ef237ca8e0
teloe <tomeloe@gmail.com>
2019-10-17 14:18:40 -0700

    style up to .dr section

--- reyes-alejandro/js/custom.js
+++ reyes-alejandro/js/custom.js
@@ -2,10 +2,18 @@ jQuery(document).ready(function ($) {
 
     $('.stellarnav').stellarNav({
         showArrows: false,
-        breakpoint: 960,
-        menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: ''
+        breakpoint: 959,
+        menuLabel: '<span>Menu</span>',
+        phoneBtn: '1-656-611-6185',
+        locationBtn: 'https://goo.gl/maps/EGdeTZWo6HDJPAzy9'
+    });
+
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 0) {
+            $('header').addClass('sticky');
+        } else {
+            $('header').removeClass('sticky');
+        }
     });
 
     /* $('#slider').flexslider({
