fb53b0453b6ddcdf5d74e7f3f878b85512168ccb
teloe <tomeloe@gmail.com>
2019-10-23 19:58:16 -0700

    begin markup/ styling

--- steele-matthew/js/custom.js
+++ steele-matthew/js/custom.js
@@ -3,9 +3,9 @@ jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
         showArrows: false,
         breakpoint: 960,
-        menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: ''
+        menuLabel: '<span>Menu</span>',
+        phoneBtn: '817-731-5330',
+        locationBtn: 'https://goo.gl/maps/P6wyw6oL9Wtokmef6'
     });
 
     /* $('#slider').flexslider({
