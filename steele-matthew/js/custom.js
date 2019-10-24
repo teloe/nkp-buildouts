4256d89b882fcfaaf2575706fb2398d8707595a6
teloe <tomeloe@gmail.com>
2019-10-24 14:24:46 -0700

    style up to .testimonials section

--- steele-matthew/js/custom.js
+++ steele-matthew/js/custom.js
@@ -2,10 +2,15 @@ jQuery(document).ready(function ($) {
 
     $('.stellarnav').stellarNav({
         showArrows: false,
-        breakpoint: 960,
+        breakpoint: 1004,
         menuLabel: '<span>Menu</span>',
         phoneBtn: '817-731-5330',
         locationBtn: 'https://goo.gl/maps/P6wyw6oL9Wtokmef6'
+    });
+
+    $('.video-overlay').on('click', function() {
+        $(this).fadeOut();
+        $('#dr-vid')[0].play();
     });
 
     /* $('#slider').flexslider({
