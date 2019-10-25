2490a18d7c06fed4c445d6ec1aa37481b14312a3
teloe <tomeloe@gmail.com>
2019-10-25 12:08:54 -0700

    final homepage edits

--- steele-matthew/js/custom.js
+++ steele-matthew/js/custom.js
@@ -8,10 +8,41 @@ jQuery(document).ready(function ($) {
         locationBtn: 'https://goo.gl/maps/P6wyw6oL9Wtokmef6'
     });
 
+    $(window).on('scroll', function(){
+        if ( $(this).scrollTop() > 250 ) {
+            $('#logo img').css('height', '96px');
+        } else {
+            $('#logo img').css('height', '144px');
+        } 
+    });
+
     $('.video-overlay').on('click', function() {
         $(this).fadeOut();
         $('#dr-vid')[0].play();
     });
+
+    $('.bxa-slider').flexslider({
+        animation: 'fade',
+        directionNav: true,
+        controlNav: true,
+        prevText: '',
+        nextText: ''
+    });
+
+    $('.bxa-slider').magnificPopup({
+		delegate: '.bxa-img',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1]
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+		}
+	});
 
     /* $('#slider').flexslider({
         animation: 'fade',
