e0c6ca46c6185b73d758415ffbf1d8a17bf47dd7
teloe <tomeloe@gmail.com>
2019-10-28 14:49:04 -0700

    add ho-tina starter files

---
+++ ho-tina/js/custom.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($) {
+
+    $('.stellarnav').stellarNav({
+        showArrows:false,
+        breakpoint:960,
+        menuLabel:'<span>Menu</span>',
+        phoneBtn:'484.254.6564',
+        locationBtn:'https://goo.gl/maps/xnb42ebNPteL9zfw8'
+    });
+
+    /* $('#slider').flexslider({
+        animation: 'fade',
+        directionNav: false,
+        controlNav: false
+    }); */
+
+});
