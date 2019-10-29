7abed1c379c21704eb8227aec1edfc551c73fe09
teloe <tomeloe@gmail.com>
2019-10-29 09:48:10 -0700

    add markup

--- ho-tina/js/custom.js
+++ ho-tina/js/custom.js
@@ -1,11 +1,11 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'<span>Menu</span>',
-        phoneBtn:'484.254.6564',
-        locationBtn:'https://goo.gl/maps/xnb42ebNPteL9zfw8'
+        showArrows: false,
+        breakpoint: 1024,
+        menuLabel: '<span>Menu</span>',
+        phoneBtn: '484.254.6564',
+        locationBtn: 'https://goo.gl/maps/xnb42ebNPteL9zfw8'
     });
 
     /* $('#slider').flexslider({
