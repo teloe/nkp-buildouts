cb36ca3ac5e3825036cff7cff28a55d0df26a056
teloe <tomeloe@gmail.com>
2019-10-29 16:25:38 -0700

    final homepage edits

--- ho-tina/js/custom.js
+++ ho-tina/js/custom.js
@@ -5,7 +5,17 @@ jQuery(document).ready(function ($) {
         breakpoint: 1024,
         menuLabel: '<span>Menu</span>',
         phoneBtn: '484.254.6564',
-        locationBtn: 'https://goo.gl/maps/xnb42ebNPteL9zfw8'
+        locationBtn: 'https://goo.gl/maps/xnb42ebNPteL9zfw8',
+        closeBtn: true,
+        closeLabel: ''
+    });
+
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 0) {
+            $('header').addClass('sticky');
+        } else {
+            $('header').removeClass('sticky');
+        }
     });
 
     /* $('#slider').flexslider({
