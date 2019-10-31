4316a4f7b344acb66c2883309a4f08c2333107e9
teloe <tomeloe@gmail.com>
2019-10-31 14:14:22 -0700

    add markup/ begin styling

--- heather-martin/js/custom.js
+++ heather-martin/js/custom.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function ($) {
 
     $('.stellarnav').stellarNav({
-        showArrows: false,
-        breakpoint: 960,
+        showArrows: true,
+        breakpoint: 1024,
         menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: ''
+        phoneBtn: '925-676-2600',
+        locationBtn: 'https://goo.gl/maps/HA4nDTBosaPr6ry36'
     });
 
     /* $('#slider').flexslider({
