c75f5e514358820774c25a0f02672824009fbd42
teloe <tomeloe@gmail.com>
2019-11-07 14:16:51 -0800

    style up to .about section

--- canoun-cary/js/custom.js
+++ canoun-cary/js/custom.js
@@ -2,16 +2,24 @@ jQuery(document).ready(function ($) {
 
     $('.stellarnav').stellarNav({
         showArrows: false,
-        breakpoint: 960,
-        menuLabel: 'Menu',
+        breakpoint: 1023,
+        menuLabel: '<span>Menu</span>',
         phoneBtn: '925-930-0888',
         locationBtn: 'https://goo.gl/maps/YHBPvgk7da8ngbzq8'
     });
 
-    $('#slider').flexslider({
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 150) {
+            $('#main-nav').addClass('sticky');
+        } else {
+            $('#main-nav').removeClass('sticky');
+        }
+    });
+
+    /* $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
         controlNav: false
-    });
+    }); */
 
 });
