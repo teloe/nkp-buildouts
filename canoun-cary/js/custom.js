4d244cb4abd869198e5f2f78c4fcb316a147916f
teloe <tomeloe@gmail.com>
2019-11-11 07:23:13 -0800

    final homepage edits

--- canoun-cary/js/custom.js
+++ canoun-cary/js/custom.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
         showArrows: false,
         breakpoint: 1023,
+        position: 'right',
         menuLabel: '<span>Menu</span>',
         phoneBtn: '925-930-0888',
         locationBtn: 'https://goo.gl/maps/YHBPvgk7da8ngbzq8'
