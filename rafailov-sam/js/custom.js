e76b25fed2db0453c4707f0f3b9e5df22518463b
teloe <tomeloe@gmail.com>
2019-11-12 14:01:20 -0800

    final homepage edits

--- rafailov-sam/js/custom.js
+++ rafailov-sam/js/custom.js
@@ -3,9 +3,18 @@ jQuery(document).ready(function($) {
     $('.stellarnav').stellarNav({
         showArrows:false,
         breakpoint:1023,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        menuLabel:'<span>Menu</span>',
+        phoneBtn:'555-949-7799',
+        locationBtn:'https://goo.gl/maps/JiDTWfHkyv36Sfk67'
+    });
+
+    // let nav = $('#main-nav').offset().top;
+    $(window).on('scroll', function() {
+        if ( $(this).scrollTop() > 200 ) {
+            $('#main-nav').addClass('sticky');
+        } else {
+            $('#main-nav').removeClass('sticky');
+        }
     });
 
     /* $('#slider').flexslider({
