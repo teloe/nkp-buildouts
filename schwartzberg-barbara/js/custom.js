6a52e5bf60b59a39229c659a62b80f220c986462
teloe <tomeloe@gmail.com>
2019-11-21 14:46:03 -0800

    style up to .services section

--- schwartzberg-barbara/js/custom.js
+++ schwartzberg-barbara/js/custom.js
@@ -9,9 +9,24 @@ jQuery(document).ready(function ($) {
         locationBtn: ''
     });
 
-    $('.menu-toggle').on('click', function () {
-        $(this).toggleClass('active');
+    $('#main-menu, #nav-overlay-close').on('click', function () {
+        $('#main-menu').toggleClass('active');
+        $('#nav-overlay-close').toggleClass('active');
         $('#main-nav').toggleClass('open');
+    });
+
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 0) {
+            $('header').addClass('sticky');    
+        } else {
+            $('header').removeClass('sticky');
+        }
+    });
+
+    $('.services-slider').flexslider({
+        animation: 'fade',
+        directionNav: false,
+        controlNav: false
     });
 
     /* $('#slider').flexslider({
