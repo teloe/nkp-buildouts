2d472926342650818d0d5a914104930c5db23222
teloe <tomeloe@gmail.com>
2019-12-02 14:49:11 -0800

    style up to .welcome section

--- butler-peter/js/custom.js
+++ butler-peter/js/custom.js
@@ -2,13 +2,22 @@ jQuery(document).ready(function($) {
 
     $('.stellarnav').stellarNav({
         showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        breakpoint:1024,
+        menuLabel:'<span>Menu</span>',
+        phoneBtn:'850-476-3223',
+        locationBtn:'https://goo.gl/maps/hggQ69mAqTCE4Wwu8'
     });
 
     $('a.btn').append('<span></span>');
+    $('.stellarnav > ul > li > a').append('<span></span>');
+
+    const sliderTop = $('#slider').offset().top;
+    $(window).on('scroll', function() {
+        $('header').addClass('sticky');
+        if ( $(this).scrollTop() < sliderTop ) {
+            $('header').removeClass('sticky');
+        }
+    });
 
     /* $('#slider').flexslider({
         animation: 'fade',
