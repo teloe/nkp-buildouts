7e36fad684774cfaaaada10dbd8acc5c9636cd13
teloe <tomeloe@gmail.com>
2019-12-13 12:52:49 -0800

    add more markup/ style updates

--- solomon-george/js/custom.js
+++ solomon-george/js/custom.js
@@ -2,10 +2,18 @@ jQuery(document).ready(function($) {
 
     $('.stellarnav').stellarNav({
         showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
+        breakpoint:1024,
+        menuLabel:'<span>Menu</span>',
         phoneBtn:'727-288-9988',
-        locationBtn:'https://goo.gl/maps/Dc1UZ1tRJu6RCxJv9'
+        locationBtn:'https://goo.gl/maps/Dc1UZ1tRJu6RCxJv9',
+        closeBtn: true
+    });
+
+    $(window).on('scroll', function() {
+        $('header').addClass('sticky');
+        if ( $(this).scrollTop() < 120 ) {
+            $('header').removeClass('sticky');
+        }
     });
 
     /* $('#slider').flexslider({
