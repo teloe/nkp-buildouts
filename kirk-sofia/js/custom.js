869c4a539f8d7107c37d9c361967329688a9c57d
teloe <tomeloe@gmail.com>
2019-12-30 14:51:35 -0800

    add jqeury.gallery.js plugin/ more style updates

--- kirk-sofia/js/custom.js
+++ kirk-sofia/js/custom.js
@@ -33,11 +33,39 @@ jQuery(document).ready(function ($) {
         manualControls: '.slider-controls li'
     });
 
+    // video carousel
+    let carousel = $('.video-carousel'),
+        items = $('.item'),
+        currdeg  = 0;
+
+    $('.next').on('click', { d: 'n' }, rotate);
+    $('.prev').on('click', { d: 'p' }, rotate);
+
+    function rotate(e){
+        if(e.data.d=='n'){
+            currdeg = currdeg - 120;
+        }
+        if(e.data.d=='p'){
+            currdeg = currdeg + 120;
+        }
+        carousel.css({
+            '-webkit-transform': 'rotateY('+currdeg+'deg)',
+            '-moz-transform': 'rotateY('+currdeg+'deg)',
+            'transform': 'rotateY('+currdeg+'deg)'
+        });
+        items.css({
+            '-webkit-transform': 'rotateY('+(-currdeg)+'deg)',
+            '-moz-transform': 'rotateY('+(-currdeg)+'deg)',
+            'transform': 'rotateY('+(-currdeg)+'deg)'
+        });
+    }
+
     /* $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
         controlNav: false
     }); */
+
 
 });
 
