e6c4a7b50e8015cc63ec9441e4633c46f3a71ed2
teloe <tomeloe@gmail.com>
2020-01-02 08:53:09 -0800

    final homepage changes

--- kirk-sofia/js/custom.js
+++ kirk-sofia/js/custom.js
@@ -33,32 +33,13 @@ jQuery(document).ready(function ($) {
         manualControls: '.slider-controls li'
     });
 
-    // video carousel
-    let carousel = $('.video-carousel'),
-        items = $('.item'),
-        currdeg  = 0;
-
-    $('.next').on('click', { d: 'n' }, rotate);
-    $('.prev').on('click', { d: 'p' }, rotate);
-
-    function rotate(e){
-        if(e.data.d=='n'){
-            currdeg = currdeg - 120;
-        }
-        if(e.data.d=='p'){
-            currdeg = currdeg + 120;
-        }
-        carousel.css({
-            '-webkit-transform': 'rotateY('+currdeg+'deg)',
-            '-moz-transform': 'rotateY('+currdeg+'deg)',
-            'transform': 'rotateY('+currdeg+'deg)'
-        });
-        items.css({
-            '-webkit-transform': 'rotateY('+(-currdeg)+'deg)',
-            '-moz-transform': 'rotateY('+(-currdeg)+'deg)',
-            'transform': 'rotateY('+(-currdeg)+'deg)'
-        });
-    }
+    $('.dg-wrapper .content .fa-play').magnificPopup({
+        items: {
+            src: '.popup'
+        },
+        type: 'iframe',
+        mainClass: 'mfp-fade'
+    });
 
     /* $('#slider').flexslider({
         animation: 'fade',
