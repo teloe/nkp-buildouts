41c71b7312d567075849e63618e07bdd1b876006
teloe <tomeloe@gmail.com>
2020-01-06 14:54:45 -0800

    add more styles

--- raskin-elsa/js/custom.js
+++ raskin-elsa/js/custom.js
@@ -3,15 +3,37 @@ jQuery(document).ready(function($) {
     $('.stellarnav').stellarNav({
         showArrows:false,
         breakpoint:1024,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        menuLabel:'<span>Menu</span>',
+        phoneBtn:'203-861-6620',
+        locationBtn:'https://goo.gl/maps/m9Dt2QHDBFARZ9n46'
     });
 
-    /* $('#slider').flexslider({
+    // sticky nav
+    function checkWidth() {
+        let lastScrollTop = 0;
+        $(window).on('scroll', function () {
+            if ($(window).width() > 1024) {
+                let st = $(this).scrollTop();
+                if (st > lastScrollTop) {
+                    $('nav').css('transform', 'translateY(-100%)');
+                } else {
+                    $('nav').css('transform', 'translateY(0)');
+                }
+                lastScrollTop = st;
+            }
+        });
+    }
+    checkWidth();
+    $(window).resize(checkWidth);
+
+    $('#procs-slider').flexslider({
         animation: 'fade',
-        directionNav: false,
-        controlNav: false
-    }); */
+        directionNav: true,
+        controlNav: true,
+        manualControls: '.controls ul li',
+        prevText: '',
+        nextText: ''
+
+    });
 
 });
