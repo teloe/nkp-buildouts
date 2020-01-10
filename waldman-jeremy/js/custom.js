7b487a496a406233308db3a354c465140df27e22
teloe <tomeloe@gmail.com>
2020-01-10 12:58:17 -0800

    more style edits

--- waldman-jeremy/js/custom.js
+++ waldman-jeremy/js/custom.js
@@ -5,8 +5,30 @@ jQuery(document).ready(function($) {
         breakpoint:1024,
         menuLabel:'<span>Menu</span>',
         phoneBtn:'603-577-5559',
-        locationBtn:'https://goo.gl/maps/mBZf7AxxB7hTLBhj9'
+        locationBtn:'https://goo.gl/maps/mBZf7AxxB7hTLBhj9',
+        closeBtn: true
     });
+
+    // sticky nav
+    function checkWidth() {
+        let lastScrollTop = 0;
+        $(window).on('scroll', function() {
+            if ($(window).width() > 1024) {
+                let st = $(this).scrollTop();
+                $('nav').css('transform', 'translateY(0)');
+                if (st > lastScrollTop) {
+                    $('nav').css('transform', 'translateY(-100%)');
+                }
+                lastScrollTop = st;
+            }
+            $('nav').addClass('bg-color');
+            if ( $(this).scrollTop() === 0 ) {
+                $('nav').removeClass('bg-color');
+            }
+        });
+    }
+    checkWidth();
+    $(window).resize(checkWidth);
 
     /* $('#slider').flexslider({
         animation: 'fade',
