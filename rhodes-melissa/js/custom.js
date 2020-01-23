2250a50c4da57227aa74cb1e177b9f43db2d2ef9
Tom Eloe <tomeloe@gmail.com>
2020-01-22 17:08:53 -0800

    begin styling/ header

--- rhodes-melissa/js/custom.js
+++ rhodes-melissa/js/custom.js
@@ -1,17 +1,16 @@
 jQuery(document).ready(function($) {
-
-    $(".stellarnav").stellarNav({
-        showArrows:false,
-        breakpoint:1024,
-        menuLabel:"<span>Menu</span>",
-        phoneBtn:"615-810-8785",
-        locationBtn:"https://goo.gl/maps/E99JvHfeGYTtzuAj7"
+    $('.stellarnav').stellarNav({
+        showArrows: false,
+        breakpoint: 1024,
+        menuLabel: 'Menu',
+        phoneBtn: '615-810-8785',
+        locationBtn: 'https://goo.gl/maps/E99JvHfeGYTtzuAj7',
+        closeBtn: true,
     });
 
-    $("#slider").flexslider({
-        animation: "fade",
+    $('#slider').flexslider({
+        animation: 'fade',
         directionNav: false,
-        controlNav: false
+        controlNav: false,
     });
-
 });
