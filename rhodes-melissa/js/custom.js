6f0521596adf246a76f1e473e763c6e7941adbb5
teloe <tomeloe@gmail.com>
2020-01-23 14:14:49 -0800

    more styles/ almost done with header/slider

--- rhodes-melissa/js/custom.js
+++ rhodes-melissa/js/custom.js
@@ -1,16 +1,25 @@
 jQuery(document).ready(function($) {
-    $('.stellarnav').stellarNav({
+    $(".stellarnav").stellarNav({
         showArrows: false,
         breakpoint: 1024,
-        menuLabel: 'Menu',
-        phoneBtn: '615-810-8785',
-        locationBtn: 'https://goo.gl/maps/E99JvHfeGYTtzuAj7',
-        closeBtn: true,
+        menuLabel: "Menu",
+        phoneBtn: "615-810-8785",
+        locationBtn: "https://goo.gl/maps/E99JvHfeGYTtzuAj7",
+        closeBtn: true
     });
 
-    $('#slider').flexslider({
-        animation: 'fade',
-        directionNav: false,
-        controlNav: false,
+    $(window).on("scroll", function() {
+        if ( $(this).scrollTop() > 350 ) {
+            $("#main-nav").addClass("fixed");
+        } else {
+            $("#main-nav").removeClass("fixed");
+        }
     });
+
+    $("#slider").flexslider({
+        animation: "fade",
+        directionNav: false,
+        controlNav: true,
+    });
+
 });
