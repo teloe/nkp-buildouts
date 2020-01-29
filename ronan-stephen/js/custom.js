4a0051fa61101e80c7f37587ff821d19f8a37c49
Tom Eloe <tomeloe@gmail.com>
2020-01-29 15:18:12 -0800

    style edits

--- ronan-stephen/js/custom.js
+++ ronan-stephen/js/custom.js
@@ -1,17 +1,24 @@
 jQuery(document).ready(function($) {
-
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        showArrows: false,
+        breakpoint: 1024,
+        menuLabel: 'Menu',
+        phoneBtn: '925-736-5757',
+        locationBtn: 'https://goo.gl/maps/X5VwjvcvCqewF8EX9',
+        closeBtn: true,
+    });
+
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 350) {
+            $('#nav-wrapper').addClass('fixed');
+        } else {
+            $('#nav-wrapper').removeClass('fixed');
+        }
     });
 
     $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
-        controlNav: false
+        controlNav: false,
     });
-
 });
