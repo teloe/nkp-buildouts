915aadbb8743abf9381f0fdb15e00ae4d10f1fb0
Tom Eloe <tomeloe@gmail.com>
2020-02-10 14:42:08 -0800

    more style updates

--- rhodes-melissa/.eslintrc.js
+++ rhodes-melissa/.eslintrc.js
@@ -1,33 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jquery": true
+    env: {
+        browser: true,
+        es6: true,
+        jquery: true,
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    extends: 'eslint:recommended',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    "parserOptions": {
-        "ecmaVersion": 2018
+    parserOptions: {
+        ecmaVersion: 2018,
     },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+    },
 };
--- rhodes-melissa/js/custom.js
+++ rhodes-melissa/js/custom.js
@@ -1,25 +1,29 @@
 jQuery(document).ready(function($) {
-    $(".stellarnav").stellarNav({
+    $('.stellarnav').stellarNav({
         showArrows: false,
         breakpoint: 1024,
-        menuLabel: "Menu",
-        phoneBtn: "615-810-8785",
-        locationBtn: "https://goo.gl/maps/E99JvHfeGYTtzuAj7",
-        closeBtn: true
+        menuLabel: 'Menu',
+        phoneBtn: '615-810-8785',
+        locationBtn: 'https://goo.gl/maps/E99JvHfeGYTtzuAj7',
+        closeBtn: true,
     });
 
-    $(window).on("scroll", function() {
-        if ( $(this).scrollTop() > 350 ) {
-            $("#main-nav").addClass("fixed");
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 350) {
+            $('#main-nav').addClass('fixed');
         } else {
-            $("#main-nav").removeClass("fixed");
+            $('#main-nav').removeClass('fixed');
         }
     });
 
-    $("#slider").flexslider({
-        animation: "fade",
+    $('#slider').flexslider({
+        animation: 'fade',
         directionNav: false,
         controlNav: true,
     });
 
+    $('.procs .content').on('click', function() {
+        $(this).removeClass('active');
+        $(this).addClass('active');
+    });
 });
