29d98445234f77d4d1063d53103b56c259c0d967
Tom Eloe <tomeloe@gmail.com>
2020-02-18 13:43:29 -0800

    begin styling header

--- toole-doug/js/custom.js
+++ toole-doug/js/custom.js
@@ -1,17 +1,23 @@
 jQuery(document).ready(function($) {
-
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        showArrows: false,
+        breakpoint: 1024,
+        menuLabel: 'Menu',
+        phoneBtn: '801-266-3113',
+        locationBtn: 'https://goo.gl/maps/eHtmXFVM2sSaGDAL8',
+        closeBtn: true,
+    });
+
+    $(window).on('scroll', function() {
+        $('nav').addClass('sticky');
+        if ($(this).scrollTop() < 500) {
+            $('nav').removeClass('sticky');
+        }
     });
 
     $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
-        controlNav: false
+        controlNav: false,
     });
-
 });
