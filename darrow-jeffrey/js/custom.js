94fc37c8ae6a59e0c977344bf3a18d22f7af6681
Tom Eloe <tomeloe@gmail.com>
2020-02-24 10:28:19 -0800

    add markup/ location and phone to js

--- darrow-jeffrey/js/custom.js
+++ darrow-jeffrey/js/custom.js
@@ -1,17 +1,15 @@
 jQuery(document).ready(function($) {
-
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        showArrows: false,
+        breakpoint: 1024,
+        menuLabel: 'Menu',
+        phoneBtn: '617-267-0710',
+        locationBtn: 'https://goo.gl/maps/jhVqq3TVGJ3YRBXt9',
     });
 
     $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
-        controlNav: false
+        controlNav: false,
     });
-
 });
