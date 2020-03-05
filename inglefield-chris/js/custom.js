0fa1478a58b4bb72d0fa2c15fbdbe5efed3a054d
Tom Eloe <tomeloe@gmail.com>
2020-03-05 14:19:40 -0800

    add markup/ hamburger css

--- inglefield-chris/js/custom.js
+++ inglefield-chris/js/custom.js
@@ -1,13 +1,26 @@
 jQuery(document).ready(function($) {
     $('.stellarnav').stellarNav({
         showArrows: false,
-        breakpoint: 1024,
+        breakpoint: 9999999,
         menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: '',
+        /* phoneBtn: '0207-487-0910',
+        locationBtn: 'https://goo.gl/maps/wUBqRaLzxrLtza8P6', */
+        mobileMode: true,
     });
 
-    $('#slider').flexslider({
+    $('.hamburger').on('click', function() {
+        $(this).toggleClass('is-active');
+        $('#main-nav').toggleClass('active');
+        $('#nav-overlay-close').toggleClass('active');
+    });
+
+    $('#nav-overlay-close').on('click', function() {
+        $('#main-nav').toggleClass('active');
+        $('.hamburger').removeClass('is-active');
+        $('#nav-overlay-close').removeClass('active');
+    });
+
+    $('#testimonials-slider').flexslider({
         animation: 'fade',
         directionNav: false,
         controlNav: false,
