99b2f47cd8050162f0ca8649c9775dabdfc05d15
Tom Eloe <tomeloe@gmail.com>
2020-03-05 16:14:40 -0800

    some style changes

--- inglefield-chris/js/custom.js
+++ inglefield-chris/js/custom.js
@@ -20,6 +20,14 @@ jQuery(document).ready(function($) {
         $('#nav-overlay-close').removeClass('active');
     });
 
+    const contentTop = $('.procs').offset().top;
+    $(window).on('scroll', function() {
+        $('.hamburger-inner').addClass('menu-blue');
+        if ($(this).scrollTop() < contentTop) {
+            $('.hamburger-inner').removeClass('menu-blue');
+        }
+    });
+
     $('#testimonials-slider').flexslider({
         animation: 'fade',
         directionNav: false,
