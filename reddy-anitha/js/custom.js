6a76379a557006f4124805a6a27080f6df042a29
Tom Eloe <tomeloe@gmail.com>
2020-03-13 12:57:41 -0700

    add html markup/ begin styling

--- reddy-anitha/js/custom.js
+++ reddy-anitha/js/custom.js
@@ -1,17 +1,26 @@
 jQuery(document).ready(function($) {
-
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        showArrows: false,
+        breakpoint: 960,
+        menuLabel: 'Menu',
+        mobileMenu: true,
     });
 
-    $('#slider').flexslider({
+    $('#main-menu').on('click touchstart', function() {
+        $(this).toggleClass('active');
+        $('#main-nav').toggleClass('active');
+        $('#nav-overlay-close').toggleClass('active');
+    });
+
+    $('#nav-overlay-close, .close-btn').on('click touchstart', function() {
+        $('#main-nav').toggleClass('active');
+        $('#main-menu').removeClass('active');
+        $('#nav-overlay-close').removeClass('active');
+    });
+
+    /* $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
-        controlNav: false
-    });
-
+        controlNav: false,
+    }); */
 });
