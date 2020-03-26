9cbff601a5ea6e910a8e57afe8e92294e6958cd0
Tom Eloe <tomeloe@gmail.com>
2020-03-26 12:22:16 -0700

    add more images/ finish course page

--- prichard-pablo/js/custom.js
+++ prichard-pablo/js/custom.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
         showArrows: false,
         breakpoint: 1024,
@@ -7,16 +7,16 @@ jQuery(document).ready(function($) {
         locationBtn:
             'https://www.google.com/search?client=firefox-b-1-d&sa=X&biw=3840&bih=2041&q=Quintero+Golf+Club,+16752+W,+AZ-74,+Peoria,+AZ+85383&npsic=0&rflfq=1&rlha=0&rllag=33843946,-112420660,564&tbm=lcl&ved=2ahUKEwjVt4DS8aToAhUpIjQIHQzKDx8QtgN6BAgLEAQ&tbs=lrf:!1m4!1u2!2m2!2m1!1e1!1m5!1u15!2m2!15m1!1shas_1wheelchair_1accessible_1entrance!4e2!2m1!1e2!3sIAE,lf:1,lf_ui:1&rldoc=1#rlfi=hd:;si:3381205720428460650,l,CjRRdWludGVybyBHb2xmIENsdWIsIDE2NzUyIFcsIEFaLTc0LCBQZW9yaWEsIEFaIDg1MzgzSM7MwE5aYAoScXVpbnRlcm8gZ29sZiBjbHViEAAQARACGAAYARgCGAMYBBgFGAYYBxgIGAkiMHF1aW50ZXJvIGdvbGYgY2x1YiAxNjc1MiB3IGF6IDc0IHBlb3JpYSBheiA4NTM4Mw,y,crTsYzB8qNc;mv:[[33.849136,-112.41786529999999],[33.8387575,-112.42345619999999]]',
         closeBtn: true,
+        closeLabel: 'Close',
+        position: 'right',
     });
 
     $('.desktop-logo a').append(
         '<img class="img-resp" src="images/logo.png" />'
     );
 
-    $('.dropdown').on('touchstart', function() {
-        $(this)
-            .find('.dropdown-content')
-            .toggleClass('active');
+    $('.dropdown').on('touchstart', function () {
+        $(this).find('.dropdown-content').toggleClass('active');
     });
 
     if ($(window).width() > 1024) {
@@ -63,13 +63,13 @@ jQuery(document).ready(function($) {
 
     $('.ranking .desc').hide();
 
-    $('.ranking .next-btn').on('click', function() {
-        $('.ranking .rank').fadeOut('slow', function() {
+    $('.ranking .next-btn').on('click', function () {
+        $('.ranking .rank').fadeOut('slow', function () {
             $('.ranking .desc').fadeIn('slow');
         });
     });
-    $('.ranking .prev-btn').on('click', function() {
-        $('.ranking .desc').fadeOut('slow', function() {
+    $('.ranking .prev-btn').on('click', function () {
+        $('.ranking .desc').fadeOut('slow', function () {
             $('.ranking .rank').fadeIn('slow');
         });
     });
