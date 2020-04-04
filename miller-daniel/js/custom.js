d9bc6e0e9d54ac6a839a8071544a8854fd0199b1
Tom Eloe <tomeloe@gmail.com>
2020-04-03 17:32:24 -0700

    begin styling/ almost done with banner

--- miller-daniel/js/custom.js
+++ miller-daniel/js/custom.js
@@ -1,17 +1,23 @@
-jQuery(document).ready(function($) {
-
+jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        showArrows: false,
+        breakpoint: 1024,
+        menuLabel: 'Menu',
+        phoneBtn: '503-667-2400',
+        locationBtn: 'https://goo.gl/maps/RbW2mEFLjUJytgc69',
+        closeBtn: true,
     });
 
-    $('#slider').flexslider({
+    $(window).on('scroll', function () {
+        $('header').addClass('scroll');
+        if ($(this).scrollTop() === 0) {
+            $('header').removeClass('scroll');
+        }
+    });
+
+    /* $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
-        controlNav: false
-    });
-
+        controlNav: false,
+    }); */
 });
