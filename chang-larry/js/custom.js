a5ab6bb9cb5edffc9b42f26414c1679b9146d4dd
Tom Eloe <tomeloe@gmail.com>
2020-04-16 09:42:24 -0700

    add chang-larry starter files/ markup

---
+++ chang-larry/js/custom.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function ($) {
+    $('.stellarnav').stellarNav({
+        showArrows: false,
+        breakpoint: 960,
+        menuLabel: 'Menu',
+        phoneBtn: '',
+        locationBtn: '',
+    });
+
+    /* $('#slider').flexslider({
+        animation: 'fade',
+        directionNav: false,
+        controlNav: false,
+    }); */
+});
