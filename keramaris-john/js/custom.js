45f3e8f4706bddda72125b83f1bffca5d2e62bcb
Tom Eloe <tomeloe@gmail.com>
2020-04-28 22:06:30 -0700

    style up through bxa section

--- keramaris-john/js/custom.js
+++ keramaris-john/js/custom.js
@@ -1,15 +1,50 @@
 jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
         showArrows: false,
-        breakpoint: 960,
+        breakpoint: 999999,
         menuLabel: 'Menu',
         phoneBtn: '',
         locationBtn: '',
+        mobileMode: true,
+    });
+    $('#open-menu').on('click', function () {
+        $('#nav-overlay').slideDown();
+        $('header').hide();
     });
 
-    $('#slider').flexslider({
-        animation: 'fade',
-        directionNav: false,
-        controlNav: false,
+    $('.close-menu').on('click', function () {
+        $('#nav-overlay').slideUp();
+        $('header').show();
     });
+
+    $('.procs a')
+        .on('mouseover', function () {
+            $('#procs-menu').stop(true).fadeIn(200);
+        })
+        .on('mouseleave', function () {
+            $('#procs-menu').stop(true).delay(500).fadeOut(200);
+        });
+    $('#procs-menu')
+        .on('mouseenter', function () {
+            $(this).stop(true).clearQueue().show();
+        })
+        .on('mouseleave', function () {
+            $(this).delay(500).fadeOut(200);
+        });
+
+    $('#bxa-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        appendArrows: $('#bxa .btn-wrapper'),
+        autoplay: true,
+        speed: 1000,
+    });
+
+    $('#bxa .slick-prev')
+        .empty()
+        .append('<img class="img-resp" src="images/left-arrow.png">');
+    $('#bxa .slick-next')
+        .empty()
+        .append('<img class="img-resp" src="images/right-arrow.png">');
 });
