a85eace75b78bd8099ad4dfdb629754a1b85670b
Tom Eloe <tomeloe@gmail.com>
2020-05-05 15:20:27 -0700

    final touches

--- pennate-manny/js/custom.js
+++ pennate-manny/js/custom.js
@@ -1,11 +1,19 @@
 jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
-        showArrows: false,
+        // showArrows: false,
         breakpoint: 1024,
-        menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: '',
-        // position: left,
+        menuLabel: '<span>Menu</span>',
+        // phoneBtn: '',
+        // locationBtn: '',
+        position: 'left',
+    });
+
+    var navTop = $('#desktop-nav').offset().top;
+    $(window).on('scroll', function () {
+        $('#desktop-nav').addClass('sticky');
+        if ($(this).scrollTop() <= navTop) {
+            $('#desktop-nav').removeClass('sticky');
+        }
     });
 
     $('#slider').flexslider({
