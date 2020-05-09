9c537716f42a40bff66925c2d186b14c7663b983
Tom Eloe <tomeloe@gmail.com>
2020-05-08 18:13:11 -0700

    style up to #services section

--- moreano-edwin/js/custom.js
+++ moreano-edwin/js/custom.js
@@ -1,17 +1,33 @@
-jQuery(document).ready(function($) {
-
+jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        showArrows: false,
+        breakpoint: 1024,
+        menuLabel: 'Menu',
+        phoneBtn: '718-478-2852',
+        locationBtn: 'https://goo.gl/maps/QnfViVd6WQwRdB1k9',
+        closeBtn: true,
     });
 
     $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
-        controlNav: false
+        controlNav: false,
     });
 
+    let $nav = $('#main-nav'),
+        $navTop = $('#main-nav').offset().top;
+    $(window).on('scroll', function () {
+        $nav.addClass('sticky');
+        if ($(this).scrollTop() < $navTop + 30) {
+            $nav.removeClass('sticky');
+        }
+    });
+
+    /* $('.hamburger').on('click', function () {
+        $(this).toggleClass('is-active');
+        $('#mobile-nav .container > ul').slideToggle(300);
+    });
+    $('#mobile-nav').on('click', '.has-sub', function () {
+        $(this).find('ul').slideToggle(200);
+    }); */
 });
