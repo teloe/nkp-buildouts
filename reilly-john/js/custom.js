6cf690c20f38a4e24669d091a668ea2b09e461a3
Tom Eloe <tomeloe@gmail.com>
2020-05-19 21:22:21 -0700

    almost done styling header/ nav/ banner

--- reilly-john/js/custom.js
+++ reilly-john/js/custom.js
@@ -1,17 +1,28 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+    $('#menu-btn').on('click', function () {
+        $(this).toggleClass('open');
+        $('#nav-overlay').toggleClass('open');
+    });
+
+    $(window).on('scroll', function () {
+        $('#nav-overlay').addClass('scroll');
+        if ($(this).scrollTop() === 0) {
+            $('#nav-overlay').removeClass('scroll');
+        }
+    });
 
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        showArrows: false,
+        breakpoint: 99999999,
+        menuLabel: 'Menu',
+        phoneBtn: '',
+        locationBtn: '',
+        mobileMode: true,
     });
 
     $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
-        controlNav: false
+        controlNav: false,
     });
-
 });
