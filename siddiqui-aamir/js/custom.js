45c57790b1fdd6f170ff25f9a335094733b696b1
Tom Eloe <tomeloe@gmail.com>
2020-06-04 16:17:25 -0700

    minor changes

--- siddiqui-aamir/js/custom.js
+++ siddiqui-aamir/js/custom.js
@@ -1,17 +1,28 @@
-jQuery(document).ready(function($) {
-
+jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        showArrows: false,
+        breakpoint: 960,
+        menuLabel: 'Menu',
+        phoneBtn: '',
+        locationBtn: '',
+    });
+
+    $(window).on('scroll', function () {
+        $('#main-nav').addClass('scroll');
+        $('#main-nav .logo')
+            .empty()
+            .append('<img class="img-resp" src="images/logo.png">');
+        if ($(this).scrollTop() === 0) {
+            $('#main-nav').removeClass('scroll');
+            $('#main-nav .logo')
+                .empty()
+                .append('<img class="img-resp" src="images/logo-white.png">');
+        }
     });
 
     $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
-        controlNav: false
+        controlNav: false,
     });
-
 });
