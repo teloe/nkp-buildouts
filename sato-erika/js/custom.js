28cb5c4faee3a594881e949c10c89be428f8b64a
Tom Eloe <tomeloe@gmail.com>
2020-06-23 16:34:42 -0700

    final homepage edits

--- sato-erika/js/custom.js
+++ sato-erika/js/custom.js
@@ -14,4 +14,33 @@ jQuery(document).ready(function ($) {
         controlNav: true,
         manualControls: '.controls li',
     });
+
+    $('.popup-video').magnificPopup({
+        type: 'iframe',
+        iframe: {
+            markup:
+                '<div class="mfp-iframe-scaler">' +
+                '<div class="mfp-close"></div>' +
+                '<iframe width="560" height="315" src="https://www.youtube.com/embed/4CY9_86SpVw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
+                '</div>',
+        },
+    });
+
+    $('#reviews-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode: true,
+        responsive: [
+            {
+                // mobile
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
 });
