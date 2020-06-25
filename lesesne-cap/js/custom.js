82561fab0637e7e3e3a4a5a6a22e35eaf0e2b1b1
Tom Eloe <tomeloe@gmail.com>
2020-06-25 14:15:56 -0700

    add lesesne-cap starter files/ markup

---
+++ lesesne-cap/js/custom.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function ($) {
+    // $('#mobile-menu')
+    /* $('.stellarnav').stellarNav({
+        showArrows:false,
+        breakpoint:960,
+        menuLabel:'Menu',
+        phoneBtn:'',
+        locationBtn:''
+    });
+
+    $('#slider').flexslider({
+        animation: 'fade',
+        directionNav: false,
+        controlNav: false
+    }); */
+});
