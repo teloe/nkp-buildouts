2edd9a780e2221d4dfa5f0f4bbb82ebcdfdb4f71
Tom Eloe <tomeloe@gmail.com>
2020-07-15 13:59:37 -0700

    final homepage edits/ no forms

--- docbookkeeping/js/custom.js
+++ docbookkeeping/js/custom.js
@@ -1,17 +1,16 @@
-jQuery(document).ready(function($) {
-
+jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        showArrows: false,
+        breakpoint: 1024,
+        menuLabel: 'Menu',
+        phoneBtn: '617-420-8300',
+        locationBtn: 'https://goo.gl/maps/XYWK8dkNdDe1Jrzz6',
+        closeBtn: true,
     });
 
     $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
-        controlNav: false
+        controlNav: false,
     });
-
 });
