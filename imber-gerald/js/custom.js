11a1b15a0f26b8079815f3bb2524cd150e04ee33
Tom Eloe <tomeloe@gmail.com>
2020-07-22 15:44:44 -0700

    final homepage edits

--- imber-gerald/js/custom.js
+++ imber-gerald/js/custom.js
@@ -3,8 +3,17 @@ jQuery(document).ready(function ($) {
         showArrows: false,
         breakpoint: 1024,
         menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: '',
+        phoneBtn: '212-472-1800',
+        locationBtn: 'https://goo.gl/maps/hevSSFtLuiWo6hJu6',
+        closeBtn: true,
+    });
+
+    var navTop = $('#main-nav').offset().top;
+    $(window).on('scroll', function () {
+        $('#main-nav').addClass('stuck');
+        if ($(this).scrollTop() < navTop) {
+            $('#main-nav').removeClass('stuck');
+        }
     });
 
     /* $('#slider').flexslider({
