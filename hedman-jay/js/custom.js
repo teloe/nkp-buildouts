9214db2d2b5252cf206897254c970b1a747fe719
Tom Eloe <tomeloe@gmail.com>
2020-07-30 17:21:25 -0700

    final homepage edits

--- hedman-jay/js/custom.js
+++ hedman-jay/js/custom.js
@@ -1,17 +1,46 @@
-jQuery(document).ready(function($) {
-
+jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        showArrows: false,
+        breakpoint: 99999,
+        menuLabel: 'Menu',
+        phoneBtn: '',
+        locationBtn: '',
+        mobileMenu: true,
     });
 
-    $('#slider').flexslider({
-        animation: 'fade',
-        directionNav: false,
-        controlNav: false
+    $(window).on('scroll', function () {
+        $('header').addClass('stuck');
+        if ($(this).scrollTop() === 0) {
+            $('header').removeClass('stuck');
+        }
     });
 
+    $('#open-menu').on('click', function () {
+        $('#main-menu').addClass('open');
+        $('#nav-overlay-close').addClass('open');
+    });
+
+    $('#close-menu, #nav-overlay-close').on('click', function () {
+        $('#main-menu').removeClass('open');
+        $('#nav-overlay-close').removeClass('open');
+    });
+
+    $('#testimonials-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                // mobile
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
 });
