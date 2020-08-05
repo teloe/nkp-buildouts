eff9c6f905da6ca9fe566dc997dd6fe4f96a8ec1
Tom Eloe <tomeloe@gmail.com>
2020-08-04 18:09:25 -0700

    final homepage edits

--- sousa-nicholas/js/custom.js
+++ sousa-nicholas/js/custom.js
@@ -1,17 +1,40 @@
-jQuery(document).ready(function($) {
-
+jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
-        showArrows:false,
-        breakpoint:960,
-        menuLabel:'Menu',
-        phoneBtn:'',
-        locationBtn:''
+        showArrows: false,
+        breakpoint: 1024,
+        menuLabel: 'Menu',
+        phoneBtn: '248-256-5223',
+        locationBtn: '123 Main St',
+        closeBtn: true,
     });
 
-    $('#slider').flexslider({
-        animation: 'fade',
-        directionNav: false,
-        controlNav: false
+    let menuTop = $('.menu-wrapper').offset().top;
+    $(window).on('scroll', () => {
+        $('.menu-wrapper').addClass('stuck');
+        if ($(this).scrollTop() < menuTop) {
+            $('.menu-wrapper').removeClass('stuck');
+        }
     });
 
+    $(window).on('scroll', () => {
+        $('#toTop').addClass('show');
+        if ($(this).scrollTop() < 300) {
+            $('#toTop').removeClass('show');
+        }
+    });
+
+    $('#toTop').on('click', (e) => {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 500);
+    });
+
+    $('#gallery-slider').flexslider({
+        animation: 'slide',
+        directionNav: true,
+        controlNav: true,
+        manualControls: '.controls ul li a',
+        prevText: '',
+        nextText: '',
+        slideshowSpeed: 5000,
+    });
 });
