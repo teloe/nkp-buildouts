f384db1ca34450664ff57372d399a53531bb0d88
Tom Eloe <tomeloe@gmail.com>
2020-08-13 13:32:31 -0700

    add markup/ style/ add nav functionality

--- park-christopher/js/custom.js
+++ park-christopher/js/custom.js
@@ -1,5 +1,34 @@
 jQuery(document).ready(function ($) {
-    $('.stellarnav').stellarNav({
+    $('#open-nav').on('click', function () {
+        $('#main-nav').addClass('open');
+        $(this).css('opacity', '0');
+    });
+
+    $('#close-nav').on('click', function () {
+        $('#main-nav').removeClass('open');
+        $('#main-nav #main-menu li.has-sub')
+            .removeClass('open')
+            .find('.sub')
+            .slideUp(300);
+        $('#open-nav').css('opacity', '1');
+    });
+
+    $(document).keyup(function (e) {
+        if (e.keyCode === 27) {
+            $('#main-nav').removeClass('open');
+            $('#main-nav #main-menu li.has-sub')
+                .removeClass('open')
+                .find('.sub')
+                .slideUp(300);
+            $('#open-nav').css('opacity', '1');
+        }
+    });
+
+    $('.open-sub').on('click', function () {
+        $(this).siblings('.sub').slideToggle(300).parent().toggleClass('open');
+    });
+
+    /* $('.stellarnav').stellarNav({
         showArrows: false,
         breakpoint: 1024,
         menuLabel: 'Menu',
@@ -11,5 +40,5 @@ jQuery(document).ready(function ($) {
         animation: 'fade',
         directionNav: false,
         controlNav: false,
-    });
+    }); */
 });
