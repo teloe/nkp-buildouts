35eb311e7e7653e684561bd13eefb6dfc10e9026
Tom Eloe <tomeloe@gmail.com>
2020-08-25 20:44:52 -0700

    more style edits

--- robb-phillip/js/custom.js
+++ robb-phillip/js/custom.js
@@ -3,8 +3,17 @@ jQuery(document).ready(function ($) {
         showArrows: false,
         breakpoint: 1024,
         menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: '',
+        phoneBtn: '470-336-1850',
+        locationBtn: 'https://goo.gl/maps/JagT79xZjsWKWBC79',
+        closeBtn: true,
+    });
+
+    let navTop = $('header .bottom').offset().top;
+    $(window).on('scroll', function () {
+        $('header .bottom').addClass('stuck');
+        if ($(this).scrollTop() < navTop) {
+            $('header .bottom').removeClass('stuck');
+        }
     });
 
     $('#slider').flexslider({
