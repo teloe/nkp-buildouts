4e4a9ed76ed25b69512afbd6648f893fdd56e38e
Tom Eloe <tomeloe@gmail.com>
2020-09-04 08:00:27 -0700

    finish styling the header/ banner / menu

--- somenek-michael/js/custom.js
+++ somenek-michael/js/custom.js
@@ -1,10 +1,32 @@
 jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
         showArrows: false,
-        breakpoint: 1024,
+        breakpoint: 9999999,
         menuLabel: 'Menu',
         phoneBtn: '',
         locationBtn: '',
+        mobileMenu: true,
+    });
+
+    $('#open-menu').on('click', function () {
+        $('#main-menu').addClass('open');
+        $('#main-menu .overlay').addClass('show');
+    });
+
+    $('#close-menu').on('click', function () {
+        $('#main-menu').removeClass('open');
+        $('#main-menu .overlay').removeClass('show');
+        $('#main-menu .stellarnav li.has-sub').removeClass('open');
+        $('#main-menu .stellarnav li.has-sub > ul').css('display', 'none');
+    });
+
+    $(document).keyup(function (e) {
+        if (e.keyCode === 27) {
+            $('#main-menu').removeClass('open');
+            $('#main-menu .overlay').removeClass('show');
+            $('#main-menu .stellarnav li.has-sub').removeClass('open');
+            $('#main-menu .stellarnav li.has-sub > ul').css('display', 'none');
+        }
     });
 
     $(window).on('scroll', function () {
@@ -13,6 +35,8 @@ jQuery(document).ready(function ($) {
             $('header').removeClass('stuck');
         }
     });
+
+    $('#menu-desktop-menu > li > ul > li').addClass('title');
 
     $('#slider').flexslider({
         animation: 'fade',
