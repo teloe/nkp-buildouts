39c58ada876c60760bc1d2b798fde277d26ca8b5
Tom Eloe <tomeloe@gmail.com>
2020-09-14 16:35:34 -0700

    final homepage edits

--- yolo-medspa/src/js/custom.js
+++ yolo-medspa/src/js/custom.js
@@ -3,8 +3,16 @@ jQuery(document).ready(function ($) {
         showArrows: false,
         breakpoint: 1024,
         menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: '',
+        phoneBtn: '203-533-4560',
+        locationBtn: 'https://goo.gl/maps/38htVErpHw3fC2aH6',
+        closeBtn: true,
+    });
+
+    $(window).on('scroll', function () {
+        $('#main-nav').addClass('stuck');
+        if ($(this).scrollTop() < 400) {
+            $('#main-nav').removeClass('stuck');
+        }
     });
 
     $('#slider').flexslider({
