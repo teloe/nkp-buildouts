bfb9ed925d164e6bd140d7453e5b785639a24e44
Tom Eloe <tomeloe@gmail.com>
2020-10-06 13:06:33 -0700

    add procedure page/ styles

--- wilder-fred/app/js/custom.js
+++ wilder-fred/app/js/custom.js
@@ -31,4 +31,25 @@ jQuery(document).ready(function ($) {
         controlNav: false,
     });
 
+    $(".accordion-content").slideUp();
+    $("ul.gdl-accordion li").each(function () {
+        $(this).children(".accordion-content").css('height', function () {
+            return $(this).height();
+        });
+        $(this).children(".accordion-head").bind("click", function () {
+            $(this).children().addClass(function () {
+                if ($(this).hasClass("open-accordion")) return "";
+                return "open-accordion";
+            });
+            if ($(this).siblings(".accordion-content").css('display') == 'none') {
+                $(this).children().addClass("open-accordion");
+            } else {
+                $(this).children().removeClass("open-accordion");
+            }
+            $(this).siblings(".accordion-content").slideToggle();
+            $(this).parent().siblings("li").children(".accordion-content").slideUp();
+            $(this).parent().siblings("li").find(".open-accordion").removeClass("open-accordion");
+        });
+    });
+
 });
