65f2a49b94d144b2ca00054b1e41295ada8c0f12
Tom Eloe <tomeloe@gmail.com>
2020-10-09 14:49:45 -0700

    final homepage edits

--- shuman-donna/app/js/custom.js
+++ shuman-donna/app/js/custom.js
@@ -3,21 +3,42 @@ jQuery(document).ready(function ($) {
         showArrows: false,
         breakpoint: 1024,
         menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: '',
+        phoneBtn: '561-250-6169',
+        locationBtn: 'https://goo.gl/maps/24gyMNErkcLy7oa2A',
+        closeBtn: true,
     });
 
-    $('#slider').flexslider({
+    $(window).on('scroll', function () {
+        $('header').addClass('stuck');
+        if ($(this).scrollTop() === 0) {
+            $('header').removeClass('stuck');
+        }
+    });
+
+    $('#banner-slider').flexslider({
         animation: 'fade',
-        directionNav: false,
+        directionNav: true,
+        controlNav: false,
+        prevText: '',
+        nextText: '',
+    });
+
+    $('#reviews-slider').flexslider({
+        animation: 'fade',
+        directionNav: true,
         controlNav: true,
+        manualControls: '.controls li',
+        prevText: '',
+        nextText: '',
     });
 
     $('.accordion-content').hide();
+    $('.accordion .panel:first-of-type .accordion-content').show();
+    $('.accordion .panel:first-of-type .accordion-title').addClass('active');
     $('.accordion-title').on('click', function () {
         $(this)
             .parent()
-            .find('.title')
+            .find('.accordion-title')
             .toggleClass('active')
             .siblings('.accordion-content')
             .slideToggle(200);
