e8a1bdd3de88ab275d70bb64b088a05067101596
Tom Eloe <tomeloe@gmail.com>
2020-10-20 14:49:22 -0700

    add markup/ images/ some styles

--- rachel-john/app/js/custom.js
+++ rachel-john/app/js/custom.js
@@ -1,15 +1,72 @@
 jQuery(document).ready(function ($) {
     $('.stellarnav').stellarNav({
         showArrows: false,
-        breakpoint: 1024,
+        breakpoint: 999999,
         menuLabel: 'Menu',
+        mobileMode: true,
         phoneBtn: '',
         locationBtn: '',
+    });
+
+    $('#open-menu').on('click', function () {
+        $('#main-menu').addClass('open');
+        $('#main-menu li.has-sub').removeClass('open');
+        $('#main-menu ul.sub-menu').css('display', 'none');
+    });
+
+    $('#close-menu').on('click', function () {
+        $('#main-menu').removeClass('open');
+    });
+
+    $(document).keyup(function (e) {
+        if (e.keyCode === 27) {
+            $('#main-menu').removeClass('open');
+        }
     });
 
     $('#slider').flexslider({
         animation: 'fade',
         directionNav: false,
         controlNav: false,
+    });
+
+    $('.accordion-content').hide();
+    $('.accordion .panel:first-of-type .accordion-content').show();
+    $('.accordion .panel:first-of-type .accordion-title').addClass('active');
+    $('.accordion-title').on('click', function () {
+        $(this)
+            .parent()
+            .find('.accordion-title')
+            .toggleClass('active')
+            .siblings('.accordion-content')
+            .slideToggle(300);
+        $(this)
+            .parent()
+            .siblings('.panel')
+            .find('.accordion-title')
+            .removeClass('active')
+            .siblings('.accordion-content')
+            .slideUp(300);
+    });
+
+    $('#reviews-carousel').slick({
+        slidesToShow: 3,
+        centerPadding: 0,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        autoplaySpeed: 8000,
+        centerMode: true,
+        responsive: [
+            {
+                // tablet & mobile
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     });
 });
