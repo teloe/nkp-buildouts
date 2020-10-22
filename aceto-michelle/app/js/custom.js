2a6f5d489c579669674a5ff10a300147bafe7eac
Tom Eloe <tomeloe@gmail.com>
2020-10-22 16:55:05 -0700

    add aceta-michelle starter files

---
+++ aceto-michelle/app/js/custom.js
@@ -0,0 +1,50 @@
+jQuery(document).ready(function ($) {
+    $('.stellarnav').stellarNav({
+        showArrows: false,
+        breakpoint: 1024,
+        menuLabel: 'Menu',
+        phoneBtn: '',
+        locationBtn: '',
+    });
+
+    const header = $('header');
+    const menuTop = header.offset().top;
+    
+    function checkTop() {
+        header.addClass('stuck');
+        if ($(window).scrollTop() < menuTop) {
+            header.removeClass('stuck');
+        }
+    }
+    checkTop();
+
+    $(window).on('scroll', checkTop);
+
+    // $('#slider').flexslider({
+    //     animation: 'fade',
+    //     directionNav: false,
+    //     controlNav: false,
+    // });
+
+    $('.carousel').slick({
+        slidesToShow: 3,
+        centerPadding: 0,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        speed: 600,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                // tablet & mobile
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+
+});
