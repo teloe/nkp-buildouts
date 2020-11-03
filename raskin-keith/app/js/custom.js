63033623889296d0d094c23402a7ea2014e01808
Tom Eloe <tomeloe@gmail.com>
2020-11-03 14:55:36 -0800

    style up to #info section

--- raskin-keith/app/js/custom.js
+++ raskin-keith/app/js/custom.js
@@ -3,13 +3,36 @@ jQuery(document).ready(function ($) {
         showArrows: false,
         breakpoint: 1024,
         menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: '',
+        phoneBtn: '212-889-8600',
+        locationBtn: 'https://goo.gl/maps/PFgjKDiFjzEfC3ZU8',
+        closeBtn: true,
     });
 
-    $('#slider').flexslider({
+    const header = $('header');
+    function checkTop() {
+        header.addClass('stuck');
+        if ($(window).scrollTop() === 0) {
+            header.removeClass('stuck');
+        }
+    }
+    checkTop();
+
+    $(window).on('scroll', checkTop);
+
+    $('.arrow').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate(
+            {
+                scrollTop: $($.attr(this, 'href')).offset().top,
+            },
+            500
+        );
+    });
+
+    $('#conditions-slider').flexslider({
         animation: 'fade',
         directionNav: false,
-        controlNav: false,
+        controlNav: true,
+        manualControls: '.controls li',
     });
 });
