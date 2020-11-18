56c2029c2df6c64e21a321013a464039dbebeee3
Tom Eloe <tomeloe@gmail.com>
2020-11-17 17:28:47 -0800

    almost done styling header/ nav/ banner

--- ikeda-marcos/app/js/custom.js
+++ ikeda-marcos/app/js/custom.js
@@ -3,8 +3,34 @@ jQuery(document).ready(function ($) {
         showArrows: false,
         breakpoint: 1024,
         menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: '',
+        phoneBtn: '281.964.0000',
+        locationBtn: 'https://goo.gl/maps/qMaeq29yxfhRpmXz5',
+        closeBtn: true,
+    });
+
+    function checkTop() {
+        const nav = $('nav');
+        const navTop = $('#main-menu').offset().top;
+        nav.addClass('stuck');
+        if ($(window).scrollTop() < navTop) {
+            nav.removeClass('stuck');
+        }
+    }
+    checkTop();
+
+    $(window).on('scroll', checkTop);
+
+    $(window).on('scroll', function () {
+        const scroll = $(window).scrollTop();
+        $('#banner > img').css({
+            transform:
+                'translate3d(-50%, -' +
+                scroll / 100 +
+                '%, 0) scale(' +
+                (100 + scroll / 20) / 100 +
+                ')',
+        });
+        // $('#banner .grid-100').css('opacity', 1 - $(this).scrollTop() / 500);
     });
 
     $('#slider').flexslider({
