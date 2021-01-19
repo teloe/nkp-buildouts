1064b2fdfcbb670c9dfd0d6a0e012ae7cff331bc
Tom Eloe <tomeloe@gmail.com>
2021-01-19 13:24:18 -0800

    final homepage edits/ finishing touches

--- sholes-arianna/app/js/custom.js
+++ sholes-arianna/app/js/custom.js
@@ -3,13 +3,28 @@ jQuery(document).ready(function ($) {
         showArrows: false,
         breakpoint: 1024,
         menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: '',
+        phoneBtn: '520-447-1772',
+        locationBtn: 'https://goo.gl/maps/eKTJ2Mmy6b8KzD5J6',
+        closeBtn: true,
     });
+
+    function checkTop() {
+        const nav = $('.stellarnav');
+        const navTop = $('#main-nav').offset().top;
+        nav.addClass('stuck');
+        if ($(this).scrollTop() < navTop) {
+            nav.removeClass('stuck');
+        }
+    }
+    checkTop();
+
+    $(window).on('scroll', checkTop);
 
     $('#slider').flexslider({
         animation: 'fade',
-        directionNav: false,
+        directionNav: true,
         controlNav: false,
+        prevText: '',
+        nextText: '',
     });
 });
