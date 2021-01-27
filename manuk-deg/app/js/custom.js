ec337938bf18597bbe9eb27e35872bf82d626e17
Tom Eloe <tomeloe@gmail.com>
2021-01-27 15:18:55 -0800

    final homepage edits

--- manuk-deg/app/js/custom.js
+++ manuk-deg/app/js/custom.js
@@ -1,23 +1,82 @@
 jQuery(document).ready(function ($) {
-    /* $('.stellarnav').stellarNav({
-        showArrows: false,
-        breakpoint: 1024,
-        menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: '',
-    }); */
-
     $('.snazzymenu').snazzyMenu({
-        homeBtn: '<img class="img-resp" src="images/logo.png">',
+        homeBtn: '<img class="img-resp" src="images/logo1.png">',
         phoneBtn: '555-555-5555',
         phoneLabel: '555-555-5555',
         locationBtn: '1234 Alphabet St, Ste 208, Boca Raton, FL 33427',
         locationLabel: '1234 Alphabet St, Ste 208, Boca Raton, FL 33427',
+        colClasses: true,
     });
 
-    $('#slider').flexslider({
+    $('.column-1').prepend('<img class="img-resp" src="../images/botox.jpg">');
+    $('.column-2').prepend(
+        '<img class="img-resp" src="../images/laser-hair.jpg">'
+    );
+    $('.column-3').prepend(
+        '<img class="img-resp" src="../images/coolsculpting.jpg">'
+    );
+    $('.column-4').prepend('<img class="img-resp" src="../images/botox.jpg">');
+
+    $('.column-5').prepend('<img class="img-resp" src="../images/proc3.jpg">');
+    $('.column-6').prepend('<img class="img-resp" src="../images/proc4.jpg">');
+
+    $('.column-7').prepend(
+        '<img class="img-resp" src="../images/slider1.jpg">'
+    );
+    $('.column-8').prepend(
+        '<img class="img-resp" src="../images/slider1.jpg">'
+    );
+    $('.column-9').prepend(
+        '<img class="img-resp" src="../images/slider1.jpg">'
+    );
+
+    // Scroll functions
+    function animateOnScroll() {
+        const windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $('.scroll').each(function () {
+            const objectBottom = $(this).offset().top + $(this).innerHeight();
+
+            $(this).removeClass('visible');
+            if (objectBottom < windowBottom) {
+                $(this).addClass('visible');
+            }
+        });
+    }
+    animateOnScroll();
+
+    $(window).on('scroll', animateOnScroll);
+
+    // #services section
+    $('#services .content').on('click mouseenter', function () {
+        $(this)
+            .addClass('active')
+            .parent()
+            .siblings()
+            .find('.content')
+            .removeClass('active');
+    });
+
+    $('#services .content').on('mouseleave', function () {
+        $(this).removeClass('active');
+    });
+
+    $(document).on('click', function (e) {
+        if ($(e.target).is('#services .content') === false) {
+            $('#services .content').removeClass('active');
+        }
+    });
+
+    $(document).keyup(function (e) {
+        if (e.keyCode === 27) {
+            $('#services .content').removeClass('active');
+        }
+    });
+
+    $('.about-slider').flexslider({
         animation: 'fade',
-        directionNav: false,
+        directionNav: true,
         controlNav: false,
+        prevText: '',
+        nextText: '',
     });
 });
