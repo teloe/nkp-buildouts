9312ce6d051c505f1cf0154c0f1d9bdeb49e21d2
Tom Eloe <tomeloe@gmail.com>
2021-02-01 17:05:49 -0800

    final homepage edits

--- tyndall-jeff/app/js/custom.js
+++ tyndall-jeff/app/js/custom.js
@@ -5,11 +5,51 @@ jQuery(document).ready(function ($) {
         menuLabel: 'Menu',
         phoneBtn: '919-567-6133',
         locationBtn: 'https://goo.gl/maps/5x2oWHnhbQLyv8r88',
+        closeBtn: true,
     });
 
-    $('#slider').flexslider({
+    // Scroll functions
+    function checkTop() {
+        const navTop = $('#main-nav').offset().top;
+        const nav = $('.stellarnav');
+        nav.addClass('stuck');
+        if ($(window).scrollTop() <= navTop) {
+            nav.removeClass('stuck');
+        }
+    }
+    checkTop();
+
+    function animateOnScroll() {
+        const windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $('.scroll').each(function () {
+            const objectBottom = $(this).offset().top + $(this).innerHeight();
+
+            $(this).removeClass('visible');
+            if (objectBottom < windowBottom) {
+                $(this).addClass('visible');
+            }
+        });
+    }
+    animateOnScroll();
+
+    $(window).on('scroll', checkTop);
+    $(window).on('scroll', animateOnScroll);
+
+    // Sliders
+    $('.services-slider').flexslider({
         animation: 'fade',
-        directionNav: false,
+        directionNav: true,
+        controlNav: true,
+        manualControls: '.controls li',
+        prevText: '',
+        nextText: '',
+    });
+
+    $('.testimonials-slider').flexslider({
+        animation: 'fade',
+        directionNav: true,
         controlNav: false,
+        prevText: '',
+        nextText: '',
     });
 });
