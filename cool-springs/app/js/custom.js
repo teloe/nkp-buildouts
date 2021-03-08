f3d7c52997a72a29fd531884a9fca06de4efbec2
Tom Eloe <tomeloe@gmail.com>
2021-03-08 13:24:57 -0800

    final homepage edits

--- cool-springs/app/js/custom.js
+++ cool-springs/app/js/custom.js
@@ -3,13 +3,44 @@ jQuery(document).ready(function ($) {
         showArrows: false,
         breakpoint: 1024,
         menuLabel: 'Menu',
-        phoneBtn: '',
-        locationBtn: '',
+        phoneBtn: '615-771-7718',
+        locationBtn: 'https://goo.gl/maps/STYBnHcPXa2nZ3yR6',
+        closeBtn: true,
     });
 
-    $('#slider').flexslider({
+    // Scroll functions
+    function checkTop() {
+        const navTop = $('#main-nav').offset().top;
+        const nav = $('.stellarnav');
+        nav.addClass('stuck');
+        if ($(window).scrollTop() <= navTop) {
+            nav.removeClass('stuck');
+        }
+    }
+    checkTop();
+
+    function animateOnScroll() {
+        const windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $('.scroll').each(function () {
+            const objectBottom = $(this).offset().top + $(this).innerHeight();
+
+            $(this).removeClass('visible');
+            if (objectBottom < windowBottom) {
+                $(this).addClass('visible');
+            }
+        });
+    }
+    animateOnScroll();
+
+    $(window).on('scroll', checkTop);
+    $(window).on('scroll', animateOnScroll);
+
+    // Sliders
+    $('.testimonials-slider').flexslider({
         animation: 'fade',
-        directionNav: false,
+        directionNav: true,
         controlNav: false,
+        prevText: '',
+        nextText: '',
     });
 });
